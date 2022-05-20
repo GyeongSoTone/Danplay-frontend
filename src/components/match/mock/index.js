5ac8eaf636a8763e9410ef136882800e642b93c4
박다은 <park.daeun@navercorp.com>
2022-05-20 19:21:11 +0900

    🤡 [#5] 목데이터 추가

---
+++ src/components/match/mock/index.js
@@ -0,0 +1,218 @@
+export const MatchData = [
+  {
+    id: 1,
+    sports: 'football',
+    maxNumberOfParticipants: 10,
+    place: '풋살경기장',
+    title: '같이 풋살해요~~',
+    content: '박다은 010-8256-2515 입니다~~',
+    startTime: '2022-05-20 09:00',
+    applyTime: '2022-05-20 09:00',
+
+    // 수정
+    participants: [
+      {
+        email: '32171550@dankook.ac.kr',
+        host: true,
+        phoneNumber: '010-8256-2515',
+        age: 25,
+      },
+      {
+        email: '32171551@dankook.ac.kr',
+        host: false,
+        phoneNumber: '010-8256-2515',
+        age: 24,
+      },
+      {
+        email: '32171550@dankook.ac.kr',
+        host: false,
+        phoneNumber: '010-8256-2515',
+        age: 23,
+      },
+      {
+        email: '32171550@dankook.ac.kr',
+        host: true,
+        phoneNumber: '010-8256-2515',
+        age: 25,
+      },
+      {
+        email: '32171551@dankook.ac.kr',
+        host: false,
+        phoneNumber: '010-8256-2515',
+        age: 24,
+      },
+      {
+        email: '32171550@dankook.ac.kr',
+        host: false,
+        phoneNumber: '010-8256-2515',
+        age: 23,
+      },
+      {
+        email: '32171550@dankook.ac.kr',
+        host: true,
+        phoneNumber: '010-8256-2515',
+        age: 25,
+      },
+      {
+        email: '32171551@dankook.ac.kr',
+        host: false,
+        phoneNumber: '010-8256-2515',
+        age: 24,
+      },
+      {
+        email: '32171550@dankook.ac.kr',
+        host: false,
+        phoneNumber: '010-8256-2515',
+        age: 23,
+      },
+    ],
+
+    // 추가
+    gender: 'male',
+    numberOfPlayer: 5,
+  },
+  {
+    id: 2,
+    sports: 'football',
+    maxNumberOfParticipants: 10,
+    place: '풋살경기장',
+    title: '같이 풋살해요~~',
+    content: '박다은 010-8256-2515 입니다~~',
+    startTime: '2022-05-20 10:00',
+    applyTime: '2022-05-20 10:00',
+
+    // 수정
+    participants: [
+      {
+        email: '32171550@dankook.ac.kr',
+        host: true,
+        phoneNumber: '010-8256-2515',
+        age: 25,
+      },
+      {
+        email: '32171551@dankook.ac.kr',
+        host: false,
+        phoneNumber: '010-8256-2515',
+        age: 24,
+      },
+      {
+        email: '32171550@dankook.ac.kr',
+        host: false,
+        phoneNumber: '010-8256-2515',
+        age: 23,
+      },
+    ],
+
+    // 추가
+    gender: 'female',
+    numberOfPlayer: 5,
+  },
+  {
+    id: 3,
+    sports: 'football',
+    maxNumberOfParticipants: 10,
+    place: '풋살경기장',
+    title: '같이 풋살해요~~',
+    content: '박다은 010-8256-2515 입니다~~',
+    startTime: '2022-05-20 11:00',
+    applyTime: '2022-05-20 11:00',
+
+    // 수정
+    participants: [
+      {
+        email: '32171550@dankook.ac.kr',
+        host: true,
+        phoneNumber: '010-8256-2515',
+        age: 25,
+      },
+      {
+        email: '32171551@dankook.ac.kr',
+        host: false,
+        phoneNumber: '010-8256-2515',
+        age: 24,
+      },
+      {
+        email: '32171550@dankook.ac.kr',
+        host: false,
+        phoneNumber: '010-8256-2515',
+        age: 23,
+      },
+    ],
+
+    // 추가
+    gender: 'female',
+    numberOfPlayer: 5,
+  },
+  {
+    id: 4,
+    sports: 'football',
+    maxNumberOfParticipants: 10,
+    place: '풋살경기장',
+    title: '같이 풋살해요~~',
+    content: '박다은 010-8256-2515 입니다~~',
+    startTime: '2022-05-21 11:00',
+    applyTime: '2022-05-21 11:00',
+
+    // 수정
+    participants: [
+      {
+        email: '32171550@dankook.ac.kr',
+        host: true,
+        phoneNumber: '010-8256-2515',
+        age: 25,
+      },
+      {
+        email: '32171551@dankook.ac.kr',
+        host: false,
+        phoneNumber: '010-8256-2515',
+        age: 24,
+      },
+      {
+        email: '32171550@dankook.ac.kr',
+        host: false,
+        phoneNumber: '010-8256-2515',
+        age: 23,
+      },
+    ],
+
+    // 추가
+    gender: 'female',
+    numberOfPlayer: 5,
+  },
+  {
+    id: 3,
+    sports: 'football',
+    maxNumberOfParticipants: 10,
+    place: '풋살경기장',
+    title: '같이 풋살해요~~',
+    content: '박다은 010-8256-2515 입니다~~',
+    startTime: '2022-05-21 13:00',
+    applyTime: '2022-05-21 13:00',
+
+    // 수정
+    participants: [
+      {
+        email: '32171550@dankook.ac.kr',
+        host: true,
+        phoneNumber: '010-8256-2515',
+        age: 25,
+      },
+      {
+        email: '32171551@dankook.ac.kr',
+        host: false,
+        phoneNumber: '010-8256-2515',
+        age: 24,
+      },
+      {
+        email: '32171550@dankook.ac.kr',
+        host: false,
+        phoneNumber: '010-8256-2515',
+        age: 23,
+      },
+    ],
+
+    // 추가
+    gender: 'male',
+    numberOfPlayer: 5,
+  },
+]
